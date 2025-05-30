361b48db649cc25e040208eae704aa3311acc5ed
manish <manis.paudel07@gmail.com>
2025-05-29 22:15:17 -0500

    setting up weather project

---
+++ src/main/java/org/example/Weather.java
@@ -0,0 +1,19 @@
+package org.example;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Weather {
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("main")
+    private String main;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("icon")
+    private String icon;
+}
