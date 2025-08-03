c142cd0a1f94e192f4a408772211bcfc43cd2f6c
manish <manis.paudel07@gmail.com>
2025-08-03 17:38:52 -0500

    add cors access for frontend

--- src/main/java/org/example/WeatherController.java
+++ src/main/java/org/example/WeatherController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/weather")
+@CrossOrigin(origins = "http://localhost:3000")
 public class WeatherController {
     @Autowired
     private WeatherService weatherService;
